e8e9732072e85f0d706dd480a466336c2f11f673
Yongzhao Yang <y683yz@gmail.com>
2025-02-26 12:11:47 +0200

    fix

--- .github/scripts/parse-blueprint.sh
+++ .github/scripts/parse-blueprint.sh
@@ -2,7 +2,7 @@
 # Requires yq (e.g., 'sudo snap install yq' or similar)
 TECH_STACK=$(yq e '.project.teck_stack' blueprint.yaml)
 BUILD_TYPE=$(yq e '.project.build_type' blueprint.yaml)
-DEPLOY_METHOD=$(yq e '.project.deployment_method' blueprint.yaml)
+DEPLOY_METHOD=$(yq e '.project.deploy_method' blueprint.yaml)
 STAGES=$(yq e '.stages | keys | join(",")' blueprint.yaml | sed 's/,/","/g' | sed 's/^/["/'| sed 's/$/"]/')
 echo "tech_stack=$TECH_STACK" >> $GITHUB_OUTPUT
 echo "build_type=$BUILD_TYPE" >> $GITHUB_OUTPUT
